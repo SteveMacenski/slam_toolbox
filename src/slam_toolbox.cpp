e9acad5dd39c501b329778faa491462a494b4fb9
stevemacenski <stevenmacenski@gmail.com>
2018-08-27 17:07:08 -0700

    add timing for live but not good if getting behind

--- src/slam_toolbox.cpp
+++ src/slam_toolbox.cpp
@@ -328,7 +328,7 @@ void SlamToolbox::PublishTransformLoop(double transform_publish_period)
     {
       boost::mutex::scoped_lock lock(map_to_odom_mutex_);
       tfB_->sendTransform(tf::StampedTransform (map_to_odom_, 
-             map_to_odom_time_ + transform_timeout_, map_frame_, odom_frame_));
+             ros::Time::now() + transform_timeout_, map_frame_, odom_frame_)); // time now from map_to_odom_time_ due to live issues but should be better rectified if running behind
     }
     r.sleep();
   }
