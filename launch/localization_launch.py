5d7cd44f9f7cf495f515a132812124255c1725ec
stevemacenski <stevenmacenski@gmail.com>
2019-10-11 12:27:35 -0700

    port merge maps launch file

--- launch/localization_launch.py
+++ launch/localization_launch.py
@@ -1,5 +1,3 @@
-"""Launch a talker and a listener."""
-
 from launch import LaunchDescription
 import launch_ros.actions
 from ament_index_python.packages import get_package_share_directory
--- launch/merge_maps_kinematic_launch.py
+++ launch/merge_maps_kinematic_launch.py
@@ -5,11 +5,8 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
-        	parameters=[
-        		get_package_share_directory("slam_toolbox") + '/config/mapper_params_offline.yaml'
-        	],
             package='slam_toolbox',
-            node_executable='sync_slam_toolbox_node',
+            node_executable='merge_maps_kinematic',
             name='slam_toolbox',
             output='screen'
         )
