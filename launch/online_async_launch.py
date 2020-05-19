fc3966c381527146d8f51c42746e0555bd743fe9
Alexey Merzlyakov <60094858+AlexeyMerzlyakov@users.noreply.github.com>
2020-05-19 19:57:20 +0300

    Add use_sim_time supprt to online sync/async slam_toolbox node (#203)

--- launch/online_async_launch.py
+++ launch/online_async_launch.py
@@ -1,17 +1,31 @@
 from launch import LaunchDescription
-import launch_ros.actions
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        launch_ros.actions.Node(
-          parameters=[
-            get_package_share_directory("slam_toolbox") + '/config/mapper_params_online_async.yaml'
-          ],
-          package='slam_toolbox',
-          node_executable='async_slam_toolbox_node',
-          name='slam_toolbox',
-          output='screen'
-        )
-    ])
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
+
+    start_async_slam_toolbox_node = Node(
+        parameters=[
+          get_package_share_directory("slam_toolbox") + '/config/mapper_params_online_async.yaml',
+          {'use_sim_time': use_sim_time}
+        ],
+        package='slam_toolbox',
+        node_executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        output='screen')
+
+    ld = LaunchDescription()
+
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(start_async_slam_toolbox_node)
+
+    return ld
