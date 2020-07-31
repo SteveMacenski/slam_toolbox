31f6bf879adbd9b5c409788043fe0e3fad87642f
stevemacenski <stevenmacenski@gmail.com>
2020-07-31 13:50:54 -0700

    missed a line

--- lib/karto_sdk/src/Mapper.cpp
+++ lib/karto_sdk/src/Mapper.cpp
@@ -390,7 +390,7 @@ namespace karto
     forEach(ScanManagerMap, &m_ScanManagers)
     {
       delete iter->second;
-      iter->second - nullptr;
+      iter->second = nullptr;
     }
 
     m_ScanManagers.clear();
