a0243ddf2ffacebe00e8efb698be4c08a0cf061c
stevemacenski <stevenmacenski@gmail.com>
2020-07-31 14:46:02 -0700

    linting

--- src/laser_utils.cpp
+++ src/laser_utils.cpp
@@ -106,8 +106,7 @@ karto::LaserRangeFinder * LaserAssistant::makeLaser(const double & mountingYaw)
   laser->SetAngularResolution(scan_.angle_increment);
 
   bool is_360_lidar = false;
-  if (std::fabs(scan_.angle_max - scan_.angle_min - 2.0 * M_PI) < 1e-1)
-  {
+  if (std::fabs(scan_.angle_max - scan_.angle_min - 2.0 * M_PI) < 1e-1) {
     is_360_lidar = true;
   }
 
