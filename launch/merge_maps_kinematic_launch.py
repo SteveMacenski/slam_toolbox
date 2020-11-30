4112e10ed2623bc12c9e8277939a8da380053cfe
joe28965 <joeptool@gmail.com>
2020-11-30 09:52:27 +0100

    changed depricated node_executable to executable

--- launch/merge_maps_kinematic_launch.py
+++ launch/merge_maps_kinematic_launch.py
@@ -6,7 +6,7 @@ def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
             package='slam_toolbox',
-            node_executable='merge_maps_kinematic',
+            executable='merge_maps_kinematic',
             name='slam_toolbox',
             output='screen'
         )
