1950c1af979434c2e4312fedb443b51d40682436
PGotzmann <16465789+PGotzmann@users.noreply.github.com>
2021-03-09 13:28:18 +0100

    fix initial scan not added to localization buffer (#333)

--- slam_toolbox/lib/karto_sdk/include/karto_sdk/Mapper.h
+++ slam_toolbox/lib/karto_sdk/include/karto_sdk/Mapper.h
@@ -1960,9 +1960,10 @@ namespace karto
     // processors
     kt_bool ProcessAtDock(LocalizedRangeScan* pScan);
     kt_bool ProcessAgainstNode(LocalizedRangeScan* pScan,  const int& nodeId);
-    kt_bool ProcessAgainstNodesNearBy(LocalizedRangeScan* pScan);
+    kt_bool ProcessAgainstNodesNearBy(LocalizedRangeScan* pScan, kt_bool addScanToLocalizationBuffer = false);
     kt_bool ProcessLocalization(LocalizedRangeScan* pScan);
     kt_bool RemoveNodeFromGraph(Vertex<LocalizedRangeScan>*);
+    void AddScanToLocalizationBuffer(LocalizedRangeScan* pScan, Vertex<LocalizedRangeScan>* scan_vertex);
     void ClearLocalizationBuffer();
 
     /**
