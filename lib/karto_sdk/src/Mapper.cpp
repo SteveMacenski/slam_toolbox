6aeea407f95be8dce18988248c179988f2f1d320
Steve Macenski <stevenmacenski@gmail.com>
2021-03-23 14:18:44 -0700

    backported to foxy 360 lidar fixes (#362)

--- lib/karto_sdk/src/Mapper.cpp
+++ lib/karto_sdk/src/Mapper.cpp
@@ -1589,7 +1589,7 @@ void MapperGraph::LinkScans(
 
   // only attach link information if the edge is new
   if (isNewEdge == true) {
-    pEdge->SetLabel(new LinkInfo(pFromScan->GetSensorPose(), rMean, rCovariance));
+    pEdge->SetLabel(new LinkInfo(pFromScan->GetCorrectedPose(), pToScan->GetCorrectedAt(rMean), rCovariance));
     if (m_pMapper->m_pScanOptimizer != NULL) {
       m_pMapper->m_pScanOptimizer->AddConstraint(pEdge);
     }
@@ -1982,7 +1982,7 @@ void MapperGraph::CorrectPoses()
       if (scan == NULL) {
         continue;
       }
-      scan->SetSensorPose(iter->second);
+      scan->SetCorrectedPoseAndUpdate(iter->second);
     }
 
     pSolver->Clear();
