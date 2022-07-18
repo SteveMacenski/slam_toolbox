cc513a98d934aa0827dac56233e4fd767a64faaf
Marc Alban <marcalban@hatchbed.com>
2022-07-17 22:03:50 -0500

    Replace TF message filter with explicit canTransform checks to avoid cases where all scan messages get filtered due to delayed tf transforms.

--- src/slam_toolbox_async.cpp
+++ src/slam_toolbox_async.cpp
@@ -35,6 +35,10 @@ void AsynchronousSlamToolbox::laserCallback(
   sensor_msgs::msg::LaserScan::ConstSharedPtr scan)
 /*****************************************************************************/
 {
+  if (!waitForTransform(scan->header.frame_id, scan->header.stamp)) {
+    return;
+  }
+
   // store scan header
   scan_header = scan->header;
   // no odom info
