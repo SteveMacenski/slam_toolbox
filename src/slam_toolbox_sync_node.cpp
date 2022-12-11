785166c6ddb37df336e6364824bddd0d24f59be8
kei1107 <clavis1107@gmail.com>
2022-12-11 16:01:26 +0000

    fix: remove manual configure

--- src/slam_toolbox_sync_node.cpp
+++ src/slam_toolbox_sync_node.cpp
@@ -42,8 +42,6 @@ int main(int argc, char ** argv)
 
   rclcpp::NodeOptions options;
   auto sync_node = std::make_shared<slam_toolbox::SynchronousSlamToolbox>(options);
-  sync_node->configure();
-  sync_node->loadPoseGraphByParams();
   rclcpp::spin(sync_node->get_node_base_interface());
   rclcpp::shutdown();
   return 0;
