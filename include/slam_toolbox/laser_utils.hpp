3a5e8b63c5a142ea46c26a9d5e5d35b631368f20
Achala Athukorala <chhathuranga@gmail.com>
2023-03-15 14:42:25 +0800

    moved laser pose reading to seperate function

--- include/slam_toolbox/laser_utils.hpp
+++ include/slam_toolbox/laser_utils.hpp
@@ -80,10 +80,12 @@ public:
     const std::string & base_frame);
   ~LaserAssistant();
   LaserMetadata toLaserMetadata(sensor_msgs::msg::LaserScan scan);
+  LaserMetadata toLaserMetadata(sensor_msgs::msg::LaserScan scan, geometry_msgs::msg::TransformStamped laser_pose);
 
 private:
   karto::LaserRangeFinder * makeLaser(const double & mountingYaw);
   bool isInverted(double & mountingYaw);
+  geometry_msgs::msg::TransformStamped readLaserPose();
 
   rclcpp::Node::SharedPtr node_;
   tf2_ros::Buffer * tf_;
