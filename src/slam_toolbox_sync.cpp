74f682e7d63eec040cd451c42bdcbfc65511c90d
Steve Macenski <stevenmacenski@gmail.com>
2024-01-23 23:31:29 +0000

    Iron update to 2.7.3 (#665)
    * fix mismatched id between rviz marker and node in pose graph (#635)
    
    * Corrected spelling mistakes in README.md (#646)
    
    * added support for ROS 2 Components (#652)
    
    * allow slam_params_file passed as launch arg (prior was hard coded) (#655)
    
    * allow slam_params_file passed as launch arg (prior was hard coded)
    
    * removed modifier comment
    
    * Merge Maps: Transformation of box points has been fixed. (#661)
    
    Co-authored-by: b2ed55dcda6ed1fb7822c27124846addcb174b73 <ozanberk.kaya@arcelik.com>
    
    * Ceres solver: check parameters exist before removing them [ros2] (#659)
    
    * Ceres solver: check parameters exist before removing them
    
    * minor update logger
    
    * bump to 2.7.3 for release
    
    ---------
    
    Co-authored-by: gene.su <47352191+gene-su@users.noreply.github.com>
    Co-authored-by: Chad Faragher <wyckster@hotmail.com>
    Co-authored-by: Antonio Brandi <34162460+AntoBrandi@users.noreply.github.com>
    Co-authored-by: slowrunner <slowrunner@users.noreply.github.com>
    Co-authored-by: Ozan Berk Kaya <oenzan94@gmail.com>
    Co-authored-by: b2ed55dcda6ed1fb7822c27124846addcb174b73 <ozanberk.kaya@arcelik.com>
    Co-authored-by: PhamNhatTan <baoden93@gmail.com>

--- src/slam_toolbox_sync.cpp
+++ src/slam_toolbox_sync.cpp
@@ -131,3 +131,10 @@ bool SynchronousSlamToolbox::deserializePoseGraphCallback(
 }
 
 }  // namespace slam_toolbox
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(slam_toolbox::SynchronousSlamToolbox)
