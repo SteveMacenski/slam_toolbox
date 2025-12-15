89e200937d185370e88db070431967b298120853
Азаров Максим <144149307+AzMax22@users.noreply.github.com>
2025-12-15 22:34:58 +0300

    Add substitutions to all launches (#824)
    * Add subs substitution to localization, online_async, online_sync launch
    
    * Add arg `slam_params_file` to lifelong launch and substitution
    
    * Add arg `use_sim_time` to  lifelong_launch.py
    
    * Fix add ParameterFile to import
    
    ---------
    
    Co-authored-by: m.azarov <m.azarov@rtc.ru>

--- launch/lifelong_launch.py
+++ launch/lifelong_launch.py
@@ -1,3 +1,5 @@
+import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
@@ -10,11 +12,14 @@ from launch_ros.actions import LifecycleNode
 from launch_ros.event_handlers import OnStateTransition
 from launch_ros.events.lifecycle import ChangeState
 from lifecycle_msgs.msg import Transition
+from launch_ros.descriptions import ParameterFile
 
 
 def generate_launch_description():
     autostart = LaunchConfiguration('autostart')
     use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
+    slam_params_file = LaunchConfiguration('slam_params_file')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -23,11 +28,29 @@ def generate_launch_description():
     declare_use_lifecycle_manager = DeclareLaunchArgument(
         'use_lifecycle_manager', default_value='false',
         description='Enable bond connection during node activation')
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
+                                   'config', 'mapper_params_lifelong.yaml'),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
 
+    # Perform substitution `$find-pkg-share`
+    slam_params_file_w_subst = ParameterFile(
+        slam_params_file,
+        allow_substs=True,
+    )
+    
     start_lifelong_slam_toolbox_node = LifecycleNode(
           parameters=[
-            get_package_share_directory("slam_toolbox") + '/config/mapper_params_lifelong.yaml',
-            {'use_lifecycle_manager': use_lifecycle_manager}
+            slam_params_file_w_subst,
+            {
+              'use_lifecycle_manager': use_lifecycle_manager,
+              'use_sim_time': use_sim_time
+            }
           ],
           package='slam_toolbox',
           executable='lifelong_slam_toolbox_node',
@@ -64,6 +87,8 @@ def generate_launch_description():
 
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_use_lifecycle_manager)
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(declare_use_sim_time_argument)
     ld.add_action(start_lifelong_slam_toolbox_node)
     ld.add_action(configure_event)
     ld.add_action(activate_event)
