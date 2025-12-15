fda886467012157d0f40960fc677b9cabfb0f68e
m.azarov <m.azarov@rtc.ru>
2025-12-15 10:27:52 +0300

    Add arg `slam_params_file` to lifelong launch and substitution

--- launch/lifelong_launch.py
+++ launch/lifelong_launch.py
@@ -1,3 +1,5 @@
+import os
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (DeclareLaunchArgument, EmitEvent, LogInfo,
@@ -15,6 +17,7 @@ from lifecycle_msgs.msg import Transition
 def generate_launch_description():
     autostart = LaunchConfiguration('autostart')
     use_lifecycle_manager = LaunchConfiguration("use_lifecycle_manager")
+    slam_params_file = LaunchConfiguration('slam_params_file')
 
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
@@ -23,10 +26,21 @@ def generate_launch_description():
     declare_use_lifecycle_manager = DeclareLaunchArgument(
         'use_lifecycle_manager', default_value='false',
         description='Enable bond connection during node activation')
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("slam_toolbox"),
+                                   'config', 'mapper_params_lifelong.yaml'),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
+    # Perform substitution `$find-pkg-share`
+    slam_params_file_w_subst = ParameterFile(
+        slam_params_file,
+        allow_substs=True,
+    )
+    
     start_lifelong_slam_toolbox_node = LifecycleNode(
           parameters=[
-            get_package_share_directory("slam_toolbox") + '/config/mapper_params_lifelong.yaml',
+            slam_params_file_w_subst,
             {'use_lifecycle_manager': use_lifecycle_manager}
           ],
           package='slam_toolbox',
@@ -64,6 +78,7 @@ def generate_launch_description():
 
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_use_lifecycle_manager)
+    ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(start_lifelong_slam_toolbox_node)
     ld.add_action(configure_event)
     ld.add_action(activate_event)
