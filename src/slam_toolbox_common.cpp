f4ed53c6eded2a97c128147030f51220cc577556
fbottarel <fabrizio.bottarel@altorobotics.ai>
2026-01-12 16:06:36 +0100

    deserialize posegraph callback now adds result to service response

--- src/slam_toolbox_common.cpp
+++ src/slam_toolbox_common.cpp
@@ -1057,6 +1057,7 @@ bool SlamToolbox::deserializePoseGraphCallback(
   if (req->match_type == slam_toolbox::srv::DeserializePoseGraph::Request::UNSET) {
     RCLCPP_ERROR(get_logger(), "Deserialization called without valid"
       " processor type set. Undefined behavior!");
+    resp->result=slam_toolbox::srv::DeserializePoseGraph::Response::RESULT_INVALID_PROCESSOR_TYPE;
     return false;
   }
 
@@ -1064,6 +1065,7 @@ bool SlamToolbox::deserializePoseGraphCallback(
 
   if (filename.empty()) {
     RCLCPP_WARN(get_logger(), "No map file given!");
+    resp->result=slam_toolbox::srv::DeserializePoseGraph::Response::RESULT_INVALID_FILENAME;
     return true;
   }
 
@@ -1078,9 +1080,11 @@ bool SlamToolbox::deserializePoseGraphCallback(
   if (!serialization::read(filename, *mapper, *dataset, shared_from_this())) {
     RCLCPP_ERROR(get_logger(), "DeserializePoseGraph: Failed to read "
       "file: %s.", filename.c_str());
+    resp->result=slam_toolbox::srv::DeserializePoseGraph::Response::RESULT_FAILED_TO_READ_FILE;
     return true;
   }
   RCLCPP_DEBUG(get_logger(), "DeserializePoseGraph: Successfully read file.");
+  resp->result=slam_toolbox::srv::DeserializePoseGraph::Response::RESULT_SUCCESS;
 
   loadSerializedPoseGraph(mapper, dataset);
   updateMap();
@@ -1104,6 +1108,7 @@ bool SlamToolbox::deserializePoseGraphCallback(
     default:
       RCLCPP_FATAL(get_logger(),
         "Deserialization called without valid processor type set.");
+      resp->result=slam_toolbox::srv::DeserializePoseGraph::Response::RESULT_INVALID_PROCESSOR_TYPE;
   }
 
   return true;
